1ff3540ec34aa86b1f3dd2b11efc5c1a4b0fcc96
Leonardo Fausto <leonardohs.fausto@gmail.com>
2026-07-23 11:15:15 -0300

    feat: replace logo symbol and favicon with masculine perfume bottle icon

--- src/components/brand/brand-mark.tsx
+++ src/components/brand/brand-mark.tsx
@@ -13,9 +13,10 @@ export function BrandMark({ compact = false, inverse = false }: BrandMarkProps) 
     >
       <span aria-hidden="true" className="brand-symbol">
         <svg viewBox="0 0 32 32">
-          <path d="M16 3.5c1.7 4.8 7.3 8.2 7.3 14a7.3 7.3 0 0 1-14.6 0c0-5.8 5.6-9.2 7.3-14Z" />
-          <path d="M12.1 18.2a4.1 4.1 0 0 0 6.8 3" />
-          <path d="M10.4 8.2 7.7 5.5M21.6 8.2l2.7-2.7" />
+          <path d="M12 4.5h8v4.5h-8z" />
+          <path d="M13.5 9v3h5V9" />
+          <path d="M7.5 12h17a1.5 1.5 0 0 1 1.5 1.5v12a2 2 0 0 1-2 2h-16a2 2 0 0 1-2-2v-12a1.5 1.5 0 0 1 1.5-1.5Z" />
+          <path d="M11.5 19.5h9" />
         </svg>
       </span>
       {!compact && (
